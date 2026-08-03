a05991076f9d58d12c0fbad1a01557de9e441344
zchengchen <zhicheng@tamu.edu>
2026-08-03 09:45:23 -0500

    Add a script to build workspace

---
+++ clone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zchengchen/FuzzingBrain-Bench ../FuzzingBrain-Bench
+git clone https://github.com/zchengchen/FuzzingBrain-Bench-answers ../FuzzingBrain-Bench-answers
+git clone https://github.com/google/oss-fuzz ../oss-fuzz
